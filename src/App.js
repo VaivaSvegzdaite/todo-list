cf403c6e288009591a7838caa742205fe81dc07b
Vaiva Svegzdaite <vaiva.svegzdaite@gmail.com>
2020-11-29 17:48:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+export default class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  // state = {
+  //   todos: [
+  //     {
+  //       id: uuid(),
+  //       title: "Take out trash",
+  //       completed: false,
+  //     },
+  //     {
+  //       id: uuid(),
+  //       title: "Dinner with ex",
+  //       completed: false,
+  //     },
+  //     {
+  //       id: uuid(),
+  //       title: "Meeting with boss",
+  //       completed: false,
+  //     },
+  //   ],
+  // };
+
+  componentDidMount() {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos?_limit=10`)
+      .then((res) => this.setState({ todos: res.data }));
+  }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) =>
+        this.setState({
+          todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+        })
+      );
+  };
+
+  // delTodo = (id) => {
+  //   this.setState({
+  //     todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+  //   });
+  // };
+
+  addTodo = (title) => {
+    axios
+      .post(`https://jsonplaceholder.typicode.com/todos/`, {
+        title,
+        completed: false,
+      })
+      .then((res) => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
+
+  // addTodo = (title) => {
+  //   const newTodo = {
+  //     id: uuid(),
+  //     title: title,
+  //     completed: false,
+  //   };
+  //   this.setState({ todos: [...this.state.todos, newTodo] });
+  // };
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ad
+        recusandae iusto quas, in vel perferendis eos blanditiis quo quibusdam
+        earum dolore magni accusamus tempore sint esse est optio rem.
+      </p>
+    </React.Fragment>
+  );
+}
